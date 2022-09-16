c4755a8a8b9b5a38ef246b58569b4709b339ccdd
samape <s.sakamoto.613@gmail.com>
2022-09-16 13:57:34 +0900

    マイページ実装とビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: :show
-  resources :items, only: :index
+  resources :users, only: [:show, :edit, :update]
+  resources :items, only: [:index, :new, :create]
   root to: 'items#index'
 end
